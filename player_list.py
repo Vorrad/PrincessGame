3c472ade5d582b00b7d0a06c6a1bfcb13350e96c
Vorrad <15217340356@163.com>
2022-06-27 17:28:14 +0800

    Update player_list.py

--- player_list.py
+++ player_list.py
@@ -1,6 +1,58 @@
 """
 This module defines data struct PlayerList.
-Other module can invoke this struct as their properties.
+Other module can invoke them as base class.
 """
+import random
+
+from common import DEFAULT_NAME_LIST
+from common import GameError
+
+
+class Player:
+    """
+    properties:
+        1. name (unique)
+    """
+    def __init__(self, name=None):
+        # May occur repetition
+        if name is None:
+            # fill num to 5 digits
+            self.name = DEFAULT_NAME_LIST[random.randint(0, len(DEFAULT_NAME_LIST)-1)] \
+                        + str(random.randint(0, 99999)).zfill(5)
+        else:
+            self.name = name
+
+    def rename(self, name):
+        self.name = name
+
 
 class PlayerList:
+    """
+    properties:
+        1. name list
+        2. player list
+    """
+    def __init__(self):
+        self.names = []
+        self.players = {}
+
+    def append(self, player: Player):
+        try:
+            if player.name in self.names:
+                raise GameError("This name has been used, please change your name")
+            self.names.append(player.name)
+            self.players[player.name] = player
+        except GameError as e:
+            print(e.arg)
+
+    def delete(self, player_name: str):
+        try:
+            if player_name not in self.names:
+                raise GameError("This name does not exist")
+            self.names.remove(player_name)
+            self.players.pop(player_name)
+        except GameError as e:
+            print(e.arg)
+
+
+
