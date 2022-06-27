e4ee716f1998b98b1ef33b479168dde37e6822e2
Vorrad <15217340356@163.com>
2022-06-27 15:36:26 +0800

    rename module

---
+++ round.py
@@ -0,0 +1,10 @@
+"""
+This module defines class 'Round'
+which represents one round in a set.
+
+It contains following properties
+    1. number of rounds to win
+    2. list of players
+    3. number of players
+    4. sequence of players
+"""
---
+++ set.py
@@ -0,0 +1,11 @@
+"""
+This module defines class 'Set'
+which represents one set in a hole multiplayer game.
+
+It contains following properties
+    1. deck on the table
+    2. player list
+    3. alive player list
+    4. active player
+"""
+
