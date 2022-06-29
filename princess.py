8f1a34dab1ca6604d54f6e8c7483348ce461c4fa
Vorrad <15217340356@163.com>
2022-06-29 22:17:01 +0800

    test: add entry point at princess.py

--- princess.py
+++ princess.py
@@ -1,6 +1,12 @@
+from set import PlayerS, Set
+
+
+def main():
+    _player_list = [PlayerS("Alice"), PlayerS("Bob"), PlayerS("Cate"), PlayerS("Dylan")]
+    s = Set()
+    s.start(_player_list)
 
 
 if __name__ == '__main__':
     # 游戏开始！
-    print("Game Start!")
-
+    main()
